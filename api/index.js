90769e82b396fdeb67e7cc520c8d14a43743d413
Prajwal Khatile <prajwalkhatile@gmail.com>
2023-12-03 20:41:29 +0530

    added .gitignore

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
 
-app.listen(4000, () => {
-  console.log(`Server is running on port 4000`);
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
